2720171da8227ab9a5d9c30a98c48c55f6c3cb31
unknown <sophia.palomo12@gmail.com>
2023-04-30 22:15:04 -0700

    added sequelie stoof to model files

--- models/Product.js
+++ models/Product.js
@@ -10,7 +10,42 @@ class Product extends Model {}
 Product.init(
   {
     // define columns
+    id:{
+      type:DataTypes.INTEGER,
+      allowNull: false,
+      primaryKey:true,
+      autoIncrement: true,
+    },
+  
+  product_name:{
+    type:DataTypes.STRING,
+    allowNull: false,
+   
   },
+  price:{
+    type:DataTypes.INTEGER,
+    allowNull: false,
+    validate: {
+      isDecimal:true,
+    
+  },
+},
+stock:{
+  type:DataTypes.DECIMAL,
+  allowNull: false,
+  defaultValue: 10,
+  validate: {
+    isNumeric:true,
+  
+},
+},
+category_id:{
+  type:DataTypes.INTEGER,
+  references: {
+    model: "category",
+  },
+},
+},
   {
     sequelize,
     timestamps: false,
